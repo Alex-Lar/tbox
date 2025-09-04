0710db336a967cbd6d1de12e9f99a314ed8eacbf
Alexander Larin <doremilarin@gmail.com>
2025-09-05 00:13:15 +0300

    build: specify path to tsconfig.vitest.json

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,20 +2,25 @@ import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'node',
-    coverage: {
-      provider: 'istanbul',
+    test: {
+        typecheck: {
+            tsconfig: './tsconfig.vitest.json',
+        },
+        include: ['**/*.{test,int.test}.{js,ts}'],
+        globals: true,
+        environment: 'node',
+        coverage: {
+            provider: 'istanbul',
+        },
     },
-  },
-  resolve: {
-    alias: {
-      '@src': path.resolve(__dirname, './src'),
-      '@application': path.resolve(__dirname, './src/application'),
-      '@core': path.resolve(__dirname, './src/core'),
-      '@infrastructure': path.resolve(__dirname, './src/infrastructure'),
-      '@shared': path.resolve(__dirname, './src/shared'),
+    resolve: {
+        alias: {
+            '@src': path.resolve(__dirname, './src'),
+            '@application': path.resolve(__dirname, './src/application'),
+            '@core': path.resolve(__dirname, './src/core'),
+            '@infrastructure': path.resolve(__dirname, './src/infrastructure'),
+            '@shared': path.resolve(__dirname, './src/shared'),
+            __tests__: path.resolve(__dirname, './__tests__'),
+        },
     },
-  },
 });

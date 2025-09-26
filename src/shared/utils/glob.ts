6279718810a0afc4d9e1c47ff2fa3f5978f4b88f
Alexander Larin <doremilarin@gmail.com>
2025-09-26 13:16:21 +0300

    refactor: add isGlobPattern shared utility

--- src/shared/utils/glob.ts
+++ src/shared/utils/glob.ts
@@ -3,3 +3,8 @@ import globParent from 'glob-parent';
 export function getParentPathFromGlobString(path: string): string {
     return globParent(path);
 }
+
+export function isGlobPattern(path: string): boolean {
+    const globPatternRegex = /[\!\?\*\[\]\{\},]/;
+    return globPatternRegex.test(path);
+}

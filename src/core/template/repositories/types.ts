66cb9bb728903ce5716328d7713fbb9cdfd085df
Alexander Larin <doremilarin@gmail.com>
2025-09-27 10:01:52 +0300

    refactor: improve TemplateRepository and CLI messaging
    - Add TemplateNotFoundError in copy() method
    - Remove TemplateRepositoryInterface
    - Update loader texts for save/delete commands

--- src/core/template/repositories/types.ts
+++
@@ -1,6 +0,0 @@
-import { AddOptions } from '@application/commands/create/index.ts';
-import Template from '../entities/template.ts';
-
-export interface TemplateRepositoryInterface {
-    save(template: Template, options: AddOptions): Promise<void>;
-}

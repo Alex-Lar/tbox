c516f35c9db54016a6afcaeae107bd6719db2967
Alexander Larin <doremilarin@gmail.com>
2025-09-30 10:40:03 +0400

    refactor: update 'handleError()' to invoke 'print()' for PrettyErrors

--- src/shared/utils/error-handler.ts
+++ src/shared/utils/error-handler.ts
@@ -1,10 +1,10 @@
 import { isPrettyError } from '@shared/guards/index.ts';
 import Logger from '@shared/utils/logger.ts';
 
-export const handleError = (err: unknown): void => {
-    if (isPrettyError(err)) {
-        Logger.error(err.formatForDisplay());
+export const handleError = (error: unknown): void => {
+    if (isPrettyError(error)) {
+        error.print();
     } else {
-        Logger.error('Unexpected error:', err);
+        Logger.error(error);
     }
 };

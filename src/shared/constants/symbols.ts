54c63d8ad53c64b9c55c68204c9fec6cb46f5102
Alexander Larin <doremilarin@gmail.com>
2025-09-30 10:38:36 +0400

    feat: add new symbols

--- src/shared/constants/symbols.ts
+++ src/shared/constants/symbols.ts
@@ -1,7 +1,10 @@
-import { styleText } from 'node:util';
+import { error, info, success, warning } from '@shared/utils/style';
 
-export const ERROR_SYMBOL = styleText('red', '×');
-export const INFO_SYMBOL = styleText('blue', 'ℹ');
+export const SUCCESS_SYMBOL = success('✔');
+export const ERROR_SYMBOL = error('✖');
+export const INFO_SYMBOL = info('ℹ');
+export const WARN_SYMBOL = warning('⚠');
+
 export const BULLET_SYMBOL = '•';
 export const ANGLE_QUOTE_SYMBOL = '›';
 export const TREE_BRANCH = '├─';
